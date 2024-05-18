52f343b49a69cfa4de5f1f196d37bde0e4fe2496
Panday Amit Ray <panday.amit.ray@gmail.com>
2024-05-18 20:20:06 +0530

    Added authentication

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
-
+Live demo at https://demo-nextjs-git-main-panday-amit-rays-projects.vercel.app/dashboard
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Next.js demo Dashboard',
+  description: 'Next.js demo application by Panday Amit Ray',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
